8371c1b74a17b0b57be9873433292b4a8c3164cb
AnujShakya123 <130181154+AnujShakya123@users.noreply.github.com>
2023-05-11 01:02:55 +0530

    initial

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args){
+      System.out.println(Product.Multiply(2,4));
+      System.out.println(Product.Multiply(5.5,6.3));
+    }
 
 }
---
+++ src/main/java/com/driver/Product.java
@@ -0,0 +1,10 @@
+package com.driver;
+
+public class Product {
+    static int Multiply(int x,int y){
+        return x*y;
+    }
+    static double Multiply(double x,double y){
+        return x*y;
+    }
+}
